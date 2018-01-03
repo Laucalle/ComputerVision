04cc01d888bcd9749e949731e6f9bed31b198467
Javi <bigfirelp@gmail.com>
2018-01-03 18:40:54 +0100

    First draft of hard example search and a bit of refactoring

--- borrador.py
+++ borrador.py
@@ -10,8 +10,8 @@ from sklearn import svm
 
 def computeGradient(img, kx, ky):
 
-	img_x = cv2.sepFilter2D(img,cv2.CV_32F,kx, ky)
-	img_y = cv2.sepFilter2D(img,cv2.CV_32F,ky, kx)
+	img_x = cv2.sepFilter2D(img, cv2.CV_32F, kx, ky, borderType=cv2.BORDER_REPLICATE)
+	img_y = cv2.sepFilter2D(img, cv2.CV_32F, ky, kx, borderType=cv2.BORDER_REPLICATE)
 	mag, angle = cv2.cartToPolar(img_x, img_y,angleInDegrees=True)
 	angle = angle%180
 	del img_x
@@ -85,9 +85,8 @@ def computeCellHistograms(border_size, magnitudes, angles, cell_size = 8, bins =
 
 # Step 3: Normalization
 
-def normalizeHistogram(histograms, block_size = 2, overlapping = 0.5):
+def normalizeHistograms(histograms, block_size = 2, overlapping = 0.5):
 	step = int(block_size*(1-overlapping))
-
 	normalized = []
 	for i in range(0,histograms.shape[0]-block_size+1, step):
 		for j in range(0,histograms.shape[1]-block_size+1, step):
@@ -98,19 +97,15 @@ def normalizeHistogram(histograms, block_size = 2, overlapping = 0.5):
 				normalized.extend((histograms[i:i+block_size, j:j+block_size]/norm).flatten())
 	return np.asarray(normalized)
 
-# Warning: this function writes data to disk
-def obtainTrainingData():
-	ky = np.asarray([[0],[1],[0]])
-	kx = np.asarray([[-1],[0],[1]])
-
-	pos_path = "/home/laura/Documentos/VC/trabajo_final/INRIAPerson/96X160H96/Train/pos"
-	neg_path = "/home/laura/Documentos/VC/trabajo_final/INRIAPerson/96X160H96/Train/neg"
+def obtainTrainingData(deriv_kernel, pos_path, neg_path):
+    kx = deriv_kernel[0]
+	ky = deriv_kernel[1]
 
 	pos_directory = os.listdir(pos_path)
 	neg_directory = os.listdir(neg_path)
 
-	decriptor_length = (15*7)*(4*9)
-	features = np.zeros((len(pos_directory) + len(neg_directory), decriptor_length))
+	descriptor_length = (15*7)*(4*9)
+	features = np.zeros((len(pos_directory) + len(neg_directory), descriptor_length))
 	labels = np.repeat(np.asarray([1,0]), [len(pos_directory),len(neg_directory)])
 	i = 0
 
@@ -121,11 +116,10 @@ def obtainTrainingData():
 
 		border_size = (img.shape[0]-128) // 2
 		histograms = computeCellHistograms(border_size, magnitudes, angles, 8)
-		descriptor = normalizeHistogram(histograms)
+		descriptor = normalizeHistograms(histograms)
 		features[i,:] = descriptor
 		h.printProgressBar(i, features.shape[0])
 		i+=1
-
 
 	for name in neg_directory:
 		img = cv2.imread(os.path.join(neg_path, name))
@@ -134,15 +128,56 @@ def obtainTrainingData():
 
 		border_size = (img.shape[0]-128) // 2
 		histograms = computeCellHistograms(border_size, magnitudes, angles, 8)
-		descriptor = normalizeHistogram(histograms)
+		descriptor = normalizeHistograms(histograms)
 		features[i,:] = descriptor
 		i+=1
 		h.printProgressBar(i, features.shape[0])
 
-	pickle.dump(labels, open("labels.pk", "wb"))
-	pickle.dump(features, open("features.pk", "wb"))
+    return features, labels
+
+# Warning: this function writes data to disk
+def extractNegativeWindows(path, classifier):
+	for name in os.listdir(path):
+		img = cv2.imread(os.path.join(path, name))
+		for i in range(10):
+			f = random.randint(0,img.shape[0]-128)
+			c = random.randint(0,img.shape[1]-64)
+			cv2.imwrite("training_negative_window/"+str(i)+"_"+name, img[f:f+128,c:c+64])
+
+def extractHardExamples(path, deriv_kernel, classifier, stride = 8):
+    n_examples = 1
+	for name in os.listdir(path):
+		img = cv2.imread(os.path.join(path, name))
+        dims = (img.shape[0]/1.2,img.shape[1]/1.2,img.shape[2])
+        pyramid = [img]
+
+        while dims[0] > 128 and dims[1] > 64:
+            pyramid.append(cv2.pyrDown(src=pyramid[-1],dstsize=dims))
+            dims = (int(dims[0]/1.2),int(dims[1]/1.2),dims[2])
+
+        for level in pyramid:
+            margin_r = int((dims[0] % 8) // 2)
+            margin_c = int((dims[1] % 8) // 2)
+            magnitudes, angles = computeGradient(level[margin_r:-margin_r,margin_c:-margin_c],*deriv_kernel)
+            histograms = computeCellHistograms(0,magnitudes,angles)
+		    for i in range(0,level.shape[0]-2*margin_r+1,stride):
+                for j in range(0,level.shape[1]-2*margin_c+1,stride):
+                    window_norm_histograms = normalizeHistograms(histograms[i:i+128,j:j+64])
+                    if classifier.predict(window_norm_histograms)[0] > 0:
+                        cv2.imwrite("hard_examples/"+str(n_examples)+".png",histograms[i:i+128,j:j+64])
+
 
 def main():
+
+	pos_train_path = "/home/laura/Documentos/VC/trabajo_final/INRIAPerson/96X160H96/Train/pos"
+	neg_train_path = "/home/laura/Documentos/VC/trabajo_final/INRIAPerson/96X160H96/Train/neg"
+
+    kx = np.asarray([[-1],[0],[1]])
+    ky = np.asarray([[0],[1],[0]])
+
+    features, labels = obtainTrainingData((kx,ky),pos_train_path,neg_train_path)
+    pickle.dump(labels, open("labels.pk", "wb"))
+	pickle.dump(features, open("features.pk", "wb"))
 	labels = pickle.load(open("labels.pk", "rb"))
 	features = pickle.load(open("features.pk", "rb"))
 
@@ -152,19 +187,7 @@ def main():
 
 	correct_answers = np.sum(np.equal(predictions,labels))
 	print("Training accuracy: %.4f" % (correct_answers/predictions.shape[0]))
-	neg_path = "/home/laura/Documentos/VC/trabajo_final/INRIAPerson/96X160H96/Train/neg"
-	neg_directory = os.listdir(neg_path)
-
-
-# Warning: this function writes data to disk
-def extractHardExamples(path, classifier):
-	for name in os.listdir(path):
-		img = cv2.imread(os.path.join(path, name))
-		for
-		for i in range(10):
-			f = random.randint(0,img.shape[0]-128)
-			c = random.randint(0,img.shape[1]-64)
-			cv2.imwrite("training_negative_window/"+str(i)+"_"+name, img[f:f+128,c:c+64])
+	neg_directory = os.listdir(neg_train_path)
 
 if __name__ == "__main__":
     main()
